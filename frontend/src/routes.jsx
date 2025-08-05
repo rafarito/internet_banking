13d5482e11655391a94d952a46bb698cf8336b22
Rafael Matos <rafaritogames@gmail.com>
2025-08-05 20:44:25 -0300

    protecting routes if user issnt logged

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,34 +1,45 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Register from "./pages/Register";
+import Register from "./pages/Register/index";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Extrato from "./pages/extrato";
 import Deposito from "./pages/Deposito";
 import Saque from "./pages/Saque";
 import Pagamento from "./pages/Pagamento";
-// import Deposit from "./pages/Deposit";
-// import Saque from "./pages/Saque";
-// import Payment from "./pages/Payment";
-// import Statement from "./pages/Statement";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login/>}/>
         <Route path="/cadastro" element={<Register/>}/>
-        <Route path="/extrato" element={<Extrato/>}/>
-        <Route path="/deposito" element={<Deposito/>}/>
-        <Route path="/saque" element={<Saque/>}/>
-        <Route path="/pagamento" element={<Pagamento/>}/>
+        <Route path="/" element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        } />
+        <Route path="/extrato" element={
+          <ProtectedRoute>
+            <Extrato/>
+          </ProtectedRoute>
+        }/>
+        <Route path="/deposito" element={
+          <ProtectedRoute>
+            <Deposito/>
+          </ProtectedRoute>
+        }/>
+        <Route path="/saque" element={
+          <ProtectedRoute>
+            <Saque/>
+          </ProtectedRoute>
+        }/>
+        <Route path="/pagamento" element={
+          <ProtectedRoute>
+            <Pagamento/>
+          </ProtectedRoute>
+        }/>
       </Routes>
     </BrowserRouter>
   );
 }
-// <Route path="/register" element={<Register />} />
-// <Route path="/login" element={<Login />} />
-// <Route path="/deposit" element={<Deposit />} />
-// <Route path="/saque" element={<Saque />} />
-// <Route path="/payment" element={<Payment />} />
-// <Route path="/statement" element={<Statement />} />
