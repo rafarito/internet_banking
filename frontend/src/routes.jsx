a5406950ad16b6e25b28b10f4606f927c9b40451
Rafael Matos <rafaritogames@gmail.com>
2025-08-05 20:55:43 -0300

    removing uncecessary path

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Register from "./pages/Register/index";
+import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Extrato from "./pages/extrato";
